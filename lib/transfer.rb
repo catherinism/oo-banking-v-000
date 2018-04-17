9fa78f3f1481e896e048015645867a616b109934
Catherine Salazar <catcreates13@gmail.com>
2018-04-17 01:57:59 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-    elseif !valid?
+    elsif !valid?
       "Transaction rejected. Please check your account balance."
 end
   end
