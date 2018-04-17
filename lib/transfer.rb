10b3427eb23b125493dc608f7fd0abadeaaf612a
Catherine Salazar <catcreates13@gmail.com>
2018-04-17 02:05:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ end
   end
 
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
       sender.balance += amount
       receiver.balance -= amount
       @status = "reversed"
