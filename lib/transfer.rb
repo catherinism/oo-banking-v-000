680adac7017b7cdec76d1540d29c94b88a500068
Catherine Salazar <catcreates13@gmail.com>
2018-04-17 01:49:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,12 +17,13 @@ class Transfer
 
   def execute_transaction
 
-    if status = "pending" && valid?
+    if @status = "pending" && valid?
       #binding.pry
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
     else
+
       "Transaction rejected. Please check your account balance."
 end
   end
