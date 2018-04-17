7e869e44cc31811e3e73e3270e69b02e3bf6980b
Catherine Salazar <catcreates13@gmail.com>
2018-04-17 01:58:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else !valid?
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
 end
   end
